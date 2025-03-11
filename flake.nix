0ce3be5a928265d1b61e4bd145e4ccae75932f8a
Danny Solivan <dark@nightconcept.net>
2025-03-10 21:51:51 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Lox interpreter in Elixir";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            erlang_27
+            elixir_1_18
+          ];
+        };
+      }
+    );
+}
